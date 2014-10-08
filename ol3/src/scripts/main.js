c14067d5197136e1df9e91eb8b4eb67627a5f36d
Jorge Sanz <xurxosanz@gmail.com>
2014-10-08 21:05:16 +0200

    first point added :-)

--- ol3/src/scripts/main.js
+++ ol3/src/scripts/main.js
@@ -7,27 +7,43 @@ var antipode = function(center){
   return ol.proj.transform([lon,lat], 'EPSG:4326', 'EPSG:3857');
 };
 
+var galiciaCenter = ol.proj.transform([-7.2, 42.7], 'EPSG:4326', 'EPSG:3857');
+
 var leftView = new ol.View({
-      center: ol.proj.transform([-7.2, 42.7], 'EPSG:4326', 'EPSG:3857'),
+      center: galiciaCenter,
       zoom: 7
 });
 
 var rightView = new ol.View({
-      center: ol.proj.transform([-7.2 + 180, -42.7], 'EPSG:4326', 'EPSG:3857'),
+      center: antipode(galiciaCenter),
       zoom: 7
 });
 
+
+// Features
+var pointFeature = new ol.Feature(new ol.geom.Point(galiciaCenter ));
+var apointFeature = new ol.Feature(new ol.geom.Point(antipode(galiciaCenter) ));
+
+// Source and vector layer
+var vectorSource = new ol.source.Vector({projection: 'EPSG:4326'});
+vectorSource.addFeatures([pointFeature]);
+var vectorLayer = new ol.layer.Vector({source: vectorSource });
+
+var aVectorSource = new ol.source.Vector({projection: 'EPSG:4326'});
+aVectorSource.addFeatures([apointFeature]);
+var aVectorLayer = new ol.layer.Vector({source: aVectorSource });
+
+
 new ol.Map({
-  target: 'map',
-  renderer: 'canvas',
-  layers: [
-      new ol.layer.Tile({
-          source: new ol.source.MapQuest({
-              layer: 'osm'
-          })
-      })
-  ],
-  view: leftView
+    target: 'map',
+    renderer: 'canvas',
+    layers: [
+          new ol.layer.Tile({
+              source: new ol.source.OSM()
+          }),
+          vectorLayer
+    ],
+    view: leftView
 });
 
 new ol.Map({
@@ -36,29 +52,37 @@ new ol.Map({
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
-        })
+        }),
+        aVectorLayer
     ],
-    view: rightView,
-    controls: []
+    view: rightView
 });
 
 
 /*
-TO DO, change this on method by bindTo and ussing 
+TO DO, change this on method by bindTo and ussing
 accessor.transform function to calculate the antipode view center
 */
+leftView.bindTo('resolution', rightView);
+leftView.bindTo('rotation', rightView);
+
+var accessor = leftView.bindTo('center', rightView);
+accessor.transform(
+    function(center) {
+      if (center)
+        return antipode(center);
+    },
+    function(center) {
+      if (center)
+        return antipode(center);
+    }
+);
 
 leftView.on('change:center',function(evt){
-  rightView.setCenter(antipode(evt.target.getCenter()));
+  pointFeature.setGeometry(new ol.geom.Point(evt.target.getCenter()));
+  apointFeature.setGeometry(new ol.geom.Point(antipode(evt.target.getCenter())));
 });
 
-leftView.on('change:resolution',function(evt){
-  rightView.setResolution(evt.target.getResolution());
-});
-
-leftView.on('change:rotation',function(evt){
-  rightView.setRotation(evt.target.getRotation());
-});
 
 
 console.log('hola' + 'juas');
