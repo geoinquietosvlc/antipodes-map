bcc27d3e783efaa3b3a53690e8aa6e92efec35c3
Pedro-Juan Ferrer <vehrka@gmail.com>
2014-10-24 13:07:56 +0200

    Created the function to get the antipodal point in Postgresql

---
+++ pgsql/funciones.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE FUNCTION gvlc_antipodes_point(geom Geometry, OUT point Geometry)
+AS $$
+DECLARE
+    X numeric;
+    Y NUMERIC;
+    stm Varchar;
+BEGIN
+    IF (ST_X(geom) > 0) 
+        THEN X:= ST_X(geom) - 180;
+        ELSE X:= ST_X(geom) + 180;
+    END IF;
+    Y:= ST_Y(geom) * -1;
+
+    stm:= E'SELECT st_geomfromtext(\'POINT(' || X || ' ' || Y || E')\')';
+    EXECUTE stm INTO point;
+END;
+$$ LANGUAGE'plpgsql' STRICT IMMUTABLE
